90febea68f185bd80a3474f2f6917812e9f60943
Revathisri S <revathisri581@gmail.com>
2026-01-14 15:37:07 +0530

    Add files via upload

---
+++ task2_simple_calculater.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main(){
+    double num1,num2;
+    char op;
+    cout<<"simple Calculator\n";
+    cout<<"Enter first number:";
+    cin>>num1;
+    
+    cout<<"Enter second number:";
+    cin>>num2;
+    
+    cout<<"Choose an operation(+,-,*,/):";
+    cin>>op;
+    switch(op){
+        case '+':
+        cout<<"Result="<< num1+num2;
+        break;
+        case  '-':
+        cout<<"Result="<<num1-num2;
+        break;
+        case '*':
+        cout<<"Result="<<num1*num2;
+        break;
+        case  '/':
+        if(num2!=0)
+        {
+            cout<<"Result="<<num1/num2;
+        }
+        else{
+            cout<<"Invalid operator";
+        }
+    }
+    return 0;
+}
