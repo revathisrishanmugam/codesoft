0b19d32e7f1b2d5f98351a49bd77085ca52be343
revathisrishanmugam <revathisri581@gmail.com>
2026-01-14 09:44:38 +0530

    Add files via upload

---
+++ task1_number_guess.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<limits>
+using namespace std;
+
+int main() 
+{
+    int number,guess;
+    srand(time(0));
+    number=rand()%100 +1;
+    cout<<"Guess a number between 1 and 100 "<< endl;
+    do{
+        while(!(cout<<" Enter your guess: " && cin>> guess))
+        {
+            cout<<"Invalid input! Please enter a number."<<endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(),'\n');
+            
+        }
+        if(guess>number){
+        cout<<"Too high!"<<endl;}
+        else if(guess<number){
+        cout<<"Too low!"<<endl;}
+        else{
+        cout<<"Congratulations! you guessed correctly"<<endl;
+        }ask
+    }while(guess!= number);
+    
+    return 0;
+    
+}
